5705d0a1567735001415ab892ac57cbab678606e
Sergei Savransky <savran46@mail.ru>
2021-09-17 00:03:17 +0300

    - add gorilla/mux, negroni packages;  - add simple http server;

--- api/handler/user_handler.go
+++ api/handler/user_handler.go
@@ -1,7 +1,11 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func UserCreateHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Testing...")
 	// TODO : implement UserCreateHandler
 }
--- api/main.go
+++ api/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/batroff/todo-back/api/handler"
 	"github.com/batroff/todo-back/internal/infrastructure/repository"
 	"github.com/batroff/todo-back/internal/usecase/user"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	_ "github.com/urfave/negroni" // Add
 	"log"
+	"net/http"
+	"time"
 )
-import _ "github.com/lib/pq"
 
 func main() {
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", "postgres", "root", "localhost", 5432, "postgres")
@@ -25,4 +30,18 @@ func main() {
 	userRepo := repository.NewUserMySQL(db)
 	_ = user.NewService(userRepo)
 	// TODO : add service handler
+
+	r := mux.NewRouter().
+		Schemes("http", "https").
+		PathPrefix("/api/")
+	r.HandlerFunc(handler.UserCreateHandler)
+
+	srv := &http.Server{
+		Addr:         "0.0.0.0:3000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+		Handler:      r.GetHandler(),
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
